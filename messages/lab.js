a5ac113cafa91aa0b99149d6503603828259849d
enaito <emily.naito@tufts.edu>
2016-03-08 10:28:28 -0500

    lab.js parses json file and displays messages unformatted

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+request = new XMLHttpRequest();
+
+function parse() {
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			data = request.responseText;
+			msg = JSON.parse(data);
+			elem = document.getElementById("messages");
+			elem.innerHTML = "";
+			for (var i = 0; i < 2; i++) {
+				elem.innerHTML = elem.innerHTML + "<p>" + msg[i]["content"] + "<span class='author'>" + msg[i]["username"] + "</span>" + "</p>";
+			}
+		}
+	};
+	request.send(null);
+}
