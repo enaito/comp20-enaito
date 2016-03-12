a57e8e190b6bb296185635aa21ee245fd2a92e46
enaito <emily.naito@tufts.edu>
2016-03-12 10:51:46 -0500

    added polyline, rounding distances to 3 decimal points

--- landmarks/map_display.js
+++ landmarks/map_display.js
@@ -7,10 +7,10 @@ var myName = "BERNADINE_RYAN";
 var request = new XMLHttpRequest();
 var me = new google.maps.LatLng(myLat, myLng);
 var myOptions = {
-            zoom: 14, // The larger the zoom number, the bigger the zoom
-            center: me,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+    zoom: 14,
+    center: me,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
 var map;
 var marker;
 var pMark;
@@ -18,17 +18,17 @@ var lMark;
 var infowindow = new google.maps.InfoWindow();
 var xhr;
 var closestLandmark;
+var pathToLandmark;
 
 function init()
 {
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     xhr = new XMLHttpRequest();
-
     getMyLocation();
 }
 
 function getMyLocation() {
-    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             //comment back in when in MA
             //myLat = position.coords.latitude;
@@ -37,7 +37,7 @@ function getMyLocation() {
         });
     }
     else {
-        alert("Geolocation is not supported by your web browser.  What a shame!");
+        alert("Geolocation is not supported by your web browser.");
     }
 }
 
@@ -57,11 +57,8 @@ function renderMap()
 	xhr.send("login=" + myName + "&lat=" + myLat + "&lng=" + myLng);
 
     me = new google.maps.LatLng(myLat, myLng);
-    
-    // Update map and go there...
     map.panTo(me);
 
-    // Create a marker
     marker = new google.maps.Marker({
         position: me,
         title: myName,
@@ -70,37 +67,48 @@ function renderMap()
     marker.setMap(map);
 
     google.maps.event.addListener(marker, 'click', function() {
-    	infowindow.setContent(marker.title + " " + closestLandmark.properties.Location_Name);
+    	infowindow.setContent("<span class='title'>Name: </span>" + marker.title + 
+    						  "<br><span class='title'>Closest Landmark: </span>" + 
+    						  closestLandmark.landmark.properties.Location_Name + 
+    						  "<br><span class='title'>Landmark Distance: </span>" + 
+    						  closestLandmark.distance + " mi");
     	infowindow.open(map,marker);
+    	pathToLandmark.setMap(map);
+    	// directionsDisplay.setMap(map);
  	});
-        
-    // Open info window on click of marker
-    // google.maps.event.addListener(marker, 'click', function() {
-    //     infowindow.setContent(marker.title);
-    //     infowindow.open(map, marker);
-    // });	
 
-	
-}
+ 	google.maps.event.addListener(infowindow,'closeclick',function(){
+		pathToLandmark.setMap(null);
+		// directionsDisplay.setMap(null);
+	});
+ }
 
 function displayPeople() {
 	var myPos = {lat: myLat, lng: myLng};
 	for (i = 0; i < vals.people.length; i++) {
+		if (vals.people[i].login == myName) {
+			continue;	// don't display me twice (as classmate and me)
+		}
 		var pos = {lat: vals.people[i].lat, lng: vals.people[i].lng};
 		pMark = new google.maps.Marker({
 			position: pos,
 			title: vals.people[i].login,
 			icon: "images/you.png",
-			content: vals.people[i].login + "<br>Distance: " + distanceBetween(pos, myPos),
+			content: "<span class='title'>Name: </span>" + 
+					  vals.people[i].login + 
+					  "<br><span class='title'>Distance: </span>" + 
+					  distanceBetween(pos, myPos) + " mi",
 			map: map
 		});
 
 		google.maps.event.addListener(pMark, 'click', (function(pMark, i) {
         	return function() {
+        		pathToLandmark.setMap(null);
+        		// directionsDisplay.setMap(null);
           		infowindow.setContent(this.content);
           		infowindow.open(this.getMap(), this);
-        }
-      })(marker, i));
+        	}
+      	})(pMark, i));
 	}
 
 }
@@ -108,7 +116,8 @@ function displayPeople() {
 function displayLandmarks() {
 	for (i = 0; i < vals.landmarks.length; i++) {
 		lMark = new google.maps.Marker({
-			position: {lat: vals.landmarks[i].geometry.coordinates[1], lng: vals.landmarks[i].geometry.coordinates[0]},
+			position: {lat: vals.landmarks[i].geometry.coordinates[1], 
+					   lng: vals.landmarks[i].geometry.coordinates[0]},
 			title: vals.landmarks[i].properties.Location_Name,
 			icon: "images/place.png",
 			content: vals.landmarks[i].properties.Details,
@@ -117,31 +126,61 @@ function displayLandmarks() {
 
 		google.maps.event.addListener(lMark, 'click', (function(lMark, i) {
         	return function() {
+        		pathToLandmark.setMap(null);
+        		// directionsDisplay.setMap(null);
           		infowindow.setContent(this.content);
           		infowindow.open(this.getMap(), this);
-        }
-      })(marker, i));
+        	}
+      	})(lMark, i));
 	}
 }
 
 function findClosestLandmark() {
 	var myPos = {lat: myLat, lng: myLng};
-	var pos = {lat: vals.landmarks[0].geometry.coordinates[1], lng: vals.landmarks[0].geometry.coordinates[0]};
+	var pos = {lat: vals.landmarks[0].geometry.coordinates[1], 
+			   lng: vals.landmarks[0].geometry.coordinates[0]};
 	var currClose = distanceBetween(pos, myPos);
 	var currCloseL = vals.landmarks[0];
 	var compClose;
+	
 	for (i = 1; i < vals.landmarks.length; i++) {
-		compClose = {lat: vals.landmarks[i].geometry.coordinates[1], lng: vals.landmarks[i].geometry.coordinates[0]};
+		compClose = {lat: vals.landmarks[i].geometry.coordinates[1], 
+					 lng: vals.landmarks[i].geometry.coordinates[0]};
 		if (compClose < currClose) {
 			currClose = compClose;
 			currCloseL = vals.landmarks[i];
 		}
 	}
-	return currCloseL;
-}
 
-function toRad(x) {
-   return x * Math.PI / 180;
+    pathToLandmark = new google.maps.Polyline({
+    	path: [{lat: myLat, lng: myLng}, 
+    		   {lat: currCloseL.geometry.coordinates[1], 
+    		   	lng: currCloseL.geometry.coordinates[0]}],
+    	geodesic: true,
+    	strokeColor: '#FF0000',
+    	strokeOpacity: 1.0,
+    	strokeWeight: 2
+  	});
+
+// directionsService = new google.maps.DirectionsService();
+// directionsDisplay= new google.maps.DirectionsRenderer();
+//       google.maps.event.addListener(map, 'click', function() {
+// 	      var request = {
+// 	        origin:me, 
+// 	        destination:new google.maps.LatLng(currCloseL.geometry.coordinates[1], currCloseL.geometry.coordinates[0]),
+// 	        travelMode: google.maps.DirectionsTravelMode.DRIVING
+// 	      };
+
+// 	      directionsService.route(request, function(response, status) {
+// 	      if (status == google.maps.DirectionsStatus.OK) {
+// 	         directionsDisplay.setDirections(response);
+// 	      }
+// 	    });
+// 	});
+
+
+
+	return {landmark: currCloseL, distance: currClose};
 }
 
 function distanceBetween(locA, locB) {
@@ -150,7 +189,7 @@ function distanceBetween(locA, locB) {
 	var lat2 = locB.lat; 
 	var lng2 = locB.lng; 
 
-	var R = 6371;
+	var R = 6371;	//km
 	var x1 = lat2-lat1;
 	var dLat = toRad(x1);  
 	var x2 = lng2-lng1;
@@ -159,9 +198,15 @@ function distanceBetween(locA, locB) {
 	        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * 
 	        Math.sin(dLng/2) * Math.sin(dLng/2);  
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-	return R * c; 
+	var distMi = (R * c) / 1.60934;	//convert to miles
+	distMi = Math.round(distMi * 1000) / 1000;	// round to 3 decimal pts
+
+	return distMi;
 }
 
+function toRad(x) {
+   return x * Math.PI / 180;
+}
 
 
 
